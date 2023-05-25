4357b7591ccf2fc2624b4fd2e94c0536b17bce17
Anushareddy5106 <83777087+Anushareddy5106@users.noreply.github.com>
2023-05-25 16:43:06 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
       if (decodedToken.exp * 1000 < new Date().getTime()) logout();
     }
     setUser(JSON.parse(localStorage.getItem("profile")));
-  }, [location,user]);
+  }, [location, logout, user]);
   const classes = useStyles();
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
